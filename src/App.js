2bfe1d3312394ec40598b6f9747661ce9e7a925f
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-03-30 10:26:43 -0600

    Resolve merge conflict

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from './Nav';
+import Terminal from './Terminal';
+import Map from './Map';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      directoryStructure: {},
+      currentLocation: {},
+      currentCommand: ''
+    }
+  }
+
+  helperObject = {
+    cd: function() {
+      //change this.state.currentLoction based on arguments -
+        // none - go to root
+        // .. - go up one level
+        // dirname - check if it is a child of this.state.currentLocation, if so, move there.
+        // file name - give "cd: <filename>: Not a directory"
+    },
+    ls: function() {
+      // return all direct descendants of this.state.currentLocation
+    },
+    pwd: function() {
+      // return this.state.currentLocation
+    },
+    touch: function() {
+      // create file in this.state.currentLocation
+        // be ready for >=1 arguments
+    },
+    mkdir: function() {
+      // create dir in this.state.currentLocation
+        // be ready for >=1 arguments
+    },
+    rm: function() {
+      //check if it was rm -rf first, then do necessary work based on rm or rm -rf
+        // be ready for >=1 arguments
+    }
+  };
+
+  handleNewCommand = (command) => {
+    this.setState({ currentCommand: command });
+
+    const commandType = command[0];
+    this.helperObject[commandType]();
+    //this.setState({ currentCommand: '' }); ???
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Nav />
+        <main>
+          <Terminal handleNewCommand={ this.handleNewCommand }/>
+          <Map />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
