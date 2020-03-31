0f26744dc50ba383d69e95cb7bb076a74e8609ce
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-03-31 10:21:46 -0600

    Add ability to CD into an existing directory, CD to root

--- src/App.js
+++ src/App.js
@@ -8,14 +8,22 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      directoryStructure: {},
-      currentLocation: {},
-      currentCommand: ''
+      directoryStructure: { turing: { classwork: {} }, personal: { } },
+      pathToCurrentLocation: [],
+      currentCommand: [],
+      // previousOutput: [],
     }
   }
 
   helperObject = {
     cd: function() {
+      if (this.state.currentCommand.length === 1) {
+        this.setState({ currentLoction: {} });
+      } else {
+        const dirToMoveTo = this.state.currentCommand[1];
+        // just pretend we will get a string of a dir name, no ".."s.
+        this.setState({ currentLoction: { dirToMoveTo: {} } });
+      }
       //change this.state.currentLoction based on arguments -
         // none - go to root
         // .. - go up one level
@@ -42,12 +50,60 @@ class App extends React.Component {
     }
   };
 
+  directChildren = () => {
+    const descendants = this.state.pathToCurrentLocation.reduce((acc, level, i) => {
+      return acc[level];
+    }, this.state.directoryStructure);
+
+    return Object.keys(descendants);
+  }
+
+  cdCommand = (commandArgs) => {
+    if (!commandArgs.length) {
+      this.setState({ pathToCurrentLocation: [] });
+    } else {
+      // just pretend we will get a string of a dir name, no ".."s.
+      if (commandArgs[0].includes('..')) {
+        // "move them up!!"
+      } else if (this.directChildren().includes(commandArgs[0])) {
+        this.state.pathToCurrentLocation.push(commandArgs[0]);
+      } else {
+        // `cd: ${commandArgs[0]}: No such file or directory`
+      }
+
+    }
+  }
+
   handleNewCommand = (command) => {
     this.setState({ currentCommand: command });
-
     const commandType = command[0];
-    this.helperObject[commandType]();
-    //this.setState({ currentCommand: '' }); ???
+    const commandArgs = command.slice(1);
+
+    // LATER: anytime a command is run, we need to store that command + its output
+    //use state previousOutput
+
+    switch (commandType) {
+      case 'cd':
+        this.cdCommand(commandArgs);
+        break;
+      case 'ls':
+        // console.log(directChildren());
+        break;
+      case 'pwd':
+        //code
+        break;
+      case 'touch':
+        //code
+        break;
+      case 'mkdir':
+        //code
+        break;
+      case 'rm':
+        //code
+        break;
+    }
+
+    this.setState({ currentCommand: '' });
   }
 
   render() {
