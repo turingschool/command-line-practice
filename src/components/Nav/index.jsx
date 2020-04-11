45193fbf970e088c958e4794fb472e1030f4b8b9
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-04-11 14:19:10 -0600

    Add scss to imports, add content to each components render

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import './Nav.scss';
 
 const Nav = () => {
-  return <h1>NAV</h1>;
+  return (
+    <nav>
+      <h1>Turing Terminal</h1>
+    </nav>
+  );
 }
 
 export default Nav;
