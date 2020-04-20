c56227373f28486dc4cd410d94e7a3d5cde713d6
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-04-20 09:48:47 -0600

    Sets up basic nav bar styles

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -5,6 +5,11 @@ const Nav = () => {
   return (
     <nav>
       <h1>Turing Terminal</h1>
+        <ul className="nav-options">
+          <li>Intro</li>
+          <li>Practice</li>
+          <li>Study</li>
+        </ul>
     </nav>
   );
 }
