02f21ddb2f0b833cc06590344deb8031bc53795d
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-04-20 10:20:59 -0600

    Implement React Router

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Nav.scss';
+import {Link} from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav>
       <h1>Turing Terminal</h1>
         <ul className="nav-options">
-          <li>Intro</li>
-          <li>Practice</li>
-          <li>Study</li>
+          <li>
+            <Link to="/intro">Intro</Link>
+          </li>
+          <li>
+            <Link to="/practice">Practice</Link>
+          </li>
+          <li>
+            <Link to="/study">Study</Link>
+          </li>
         </ul>
     </nav>
   );
