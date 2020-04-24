04934b47b6d5bb2d15df6806bdbf1ae0a99f1339
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-04-23 21:48:58 -0600

    Add collapse/expand functionality on each command card

--- src/components/CommandCard/index.jsx
+++ src/components/CommandCard/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './CommandCard.scss';
 
-const CommandCard = ({command}) => {
+class CommandCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+    }
+  }
 
-  const displayCommandSnippets = (code) => {
+  displayCommandSnippets = (code) => {
     return code.map(code => {
       if (code.command) {
         return <p className="how-code">$ {code.command}</p>
@@ -13,38 +19,50 @@ const CommandCard = ({command}) => {
     });
   }
 
-  const displayExplanations = (explanations) => {
+  displayExplanations = (explanations) => {
     return explanations.map(part => {
       if (part.text) {
         return <p className="how-text">{part.text}</p>
       } else {
         return (
           <div className="command-card-example">
-            {displayCommandSnippets(part.code)}
-            <p className="how-code">$ </p>
+          {this.displayCommandSnippets(part.code)}
+          <p className="how-code">$ </p>
           </div>
         );
       }
     });
   }
 
-  return (
-    <section className="command-card">
-      <div className="command-card-header">
-        <h2>{command.commandName}</h2>
-        <h2>^</h2>
-      </div>
-      <div className="command-card-content">
-        <h3>Why would I use this command?</h3>
-        <p>{command.why1}</p>
-        <p>{command.why2}</p>
-        <h3>How do I use this command?</h3>
-        <div>{displayExplanations(command.how)}</div>
-        <h3>What are the common mistakes made with this command?</h3>
-        <p>{displayExplanations(command.mistakes)}</p>
-      </div>
-    </section>
-  );
+  toggleExpand = () => {
+    this.setState({isExpanded: !this.state.isExpanded});
+  }
+
+  render() {
+    const {command} = this.props;
+
+    return (
+      <section className="command-card">
+        <div
+          className="command-card-header"
+          onClick={this.toggleExpand}
+        >
+          <h2>{command.commandName}</h2>
+          <h2>^</h2>
+        </div>
+        <div className={this.state.isExpanded ? "command-card-content" : "hide"}>
+          <h3>Why would I use this command?</h3>
+          <p>{command.why1}</p>
+          <p>{command.why2}</p>
+          <h3>How do I use this command?</h3>
+          <div>{this.displayExplanations(command.how)}</div>
+          <h3>What are the common mistakes made with this command?</h3>
+          <div>{this.displayExplanations(command.mistakes)}</div>
+        </div>
+      </section>
+    );
+  }
 }
+
 
 export default CommandCard;
