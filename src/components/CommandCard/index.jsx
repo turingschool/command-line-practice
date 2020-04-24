da0e78ce4c79baaef767bb10c59db76368905cf3
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-04-24 12:44:23 -0600

    Clean up CommandCard file after refactor

--- src/components/CommandCard/index.jsx
+++ src/components/CommandCard/index.jsx
@@ -21,8 +21,8 @@ const CommandCard = ({command}) => {
       } else {
         return (
           <div className="command-card-example">
-          {displayCommandSnippets(part.code)}
-          <p className="how-code">$ </p>
+            {displayCommandSnippets(part.code)}
+            <p className="how-code">$ </p>
           </div>
         );
       }
@@ -52,73 +52,7 @@ const CommandCard = ({command}) => {
         <div>{displayExplanations(command.mistakes)}</div>
       </div>
     </section>
-  )
+  );
 }
-//
-// class CommandCard extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isExpanded: false,
-//     }
-//   }
-//
-//   displayCommandSnippets = (code) => {
-//     return code.map(code => {
-//       if (code.command) {
-//         return <p className="how-code">$ {code.command}</p>
-//       } else {
-//         return <p className="how-code">{code.output}</p>
-//       }
-//     });
-//   }
-//
-//   displayExplanations = (explanations) => {
-//     return explanations.map(part => {
-//       if (part.text) {
-//         return <p className="how-text">{part.text}</p>
-//       } else {
-//         return (
-//           <div className="command-card-example">
-//           {this.displayCommandSnippets(part.code)}
-//           <p className="how-code">$ </p>
-//           </div>
-//         );
-//       }
-//     });
-//   }
-//
-//   toggleExpand = () => {
-//     this.setState(({isExpanded}) => {
-//       return {isExpanded: !isExpanded}
-//     });
-//   }
-//
-//   render() {
-//     const {command} = this.props;
-//
-//     return (
-//       <section className="command-card">
-//         <div
-//           className="command-card-header"
-//           onClick={this.toggleExpand}
-//         >
-//           <h2>{command.commandName}</h2>
-//           <h2>^</h2>
-//         </div>
-//         <div className={this.state.isExpanded ? "command-card-content" : "hide"}>
-//           <h3>Why would I use this command?</h3>
-//           <p>{command.why1}</p>
-//           <p>{command.why2}</p>
-//           <h3>How do I use this command?</h3>
-//           <div>{this.displayExplanations(command.how)}</div>
-//           <h3>What are the common mistakes made with this command?</h3>
-//           <div>{this.displayExplanations(command.mistakes)}</div>
-//         </div>
-//       </section>
-//     );
-//   }
-// }
-
 
 export default CommandCard;
