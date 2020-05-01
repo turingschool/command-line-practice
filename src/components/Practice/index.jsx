87d812a02a8432ac37071f0e76f83eeb34875016
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-05-01 07:25:13 -0600

    Resolve merge conflict in Practice component

--- src/components/Practice/index.jsx
+++ src/components/Practice/index.jsx
@@ -6,14 +6,17 @@ import './Practice.scss';
 
 const Practice = ({handleNewCommand, mapData, currentExplanation, directoryStructure, currentPath}) => {
   return (
-    <main>
-      <Terminal handleNewCommand={handleNewCommand}/>
-      <Map
-        mapData={mapData}
-        currentExplanation={currentExplanation}
-        directoryStructure={directoryStructure}
-        currentPath={currentPath}
-      />
+    <main className="practice-main">
+      <h2>Practice</h2>
+      <p className="how-to-practice">Use the commands you've learned in the terminal window below. As you run each command, you'll see the visualization of the directory structure update. The directory with the rainbow background is your current working directory.</p>
+      <div className="terminal-map-container">
+        <Terminal handleNewCommand={handleNewCommand}/>
+        <Map
+          mapData={mapData}
+          currentExplanation={currentExplanation}
+          directoryStructure={directoryStructure}
+        />
+      </div>
     </main>
   );
 }
