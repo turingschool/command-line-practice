7614c3d39721224e6ab53962022286c9356859ac
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-05-03 09:56:42 -0600

    Add solutions for challenges

---
+++ src/solutions.js
@@ -0,0 +1,34 @@
+const solutions = [
+  [
+    {title: "root", type: "dir", levelFromRoot: 0 },
+    {title: "turing", type: "dir", levelFromRoot: 1 },
+    {title: "0mod", type: "dir", levelFromRoot: 2 },
+    {title: "personal", type: "dir", levelFromRoot: 1 }
+  ],
+  [
+    {title: "root", type: "dir", levelFromRoot: 0 },
+    {title: "turing", type: "dir", levelFromRoot: 1 },
+    {title: "0mod", type: "dir", levelFromRoot: 2 },
+    {title: "practice", type: "dir", levelFromRoot: 3 },
+    {title: "capstone", type: "dir", levelFromRoot: 3 },
+    {title: "1mod", type: "dir", levelFromRoot: 2 },
+    {title: "personal", type: "dir", levelFromRoot: 1 },
+    {title: "budget.txt", type: "file", levelFromRoot: 2 },
+    {title: "letter.txt", type: "file", levelFromRoot: 2 },
+  ],
+  [
+    {title: "root", type: "dir", levelFromRoot: 0 },
+    {title: "turing", type: "dir", levelFromRoot: 1 },
+    {title: "0mod", type: "dir", levelFromRoot: 2 },
+    {title: "practice", type: "dir", levelFromRoot: 3 },
+    {title: "game.rb", type: "file", levelFromRoot: 4 },
+    {title: "capstone", type: "dir", levelFromRoot: 3 },
+    {title: "1mod", type: "dir", levelFromRoot: 2 },
+    {title: "classwork", type: "dir", levelFromRoot: 3 },
+    {title: "practice", type: "dir", levelFromRoot: 3 },
+    {title: "2mod", type: "dir", levelFromRoot: 2 },
+    {title: "3mod", type: "dir", levelFromRoot: 2 },
+  ],
+];
+
+export default solutions;
