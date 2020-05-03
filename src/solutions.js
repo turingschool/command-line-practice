cb14a7982f3f79bc5a844c24c27fa18074506fa9
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-05-03 17:09:27 -0600

    Update solutions and start level

--- src/solutions.js
+++ src/solutions.js
@@ -1,29 +1,28 @@
 const solutions = [
   [
+    {title: "root", type: "dir", levelFromRoot: 0 },
     {title: "turing", type: "dir", levelFromRoot: 1 },
     {title: "0mod", type: "dir", levelFromRoot: 2 },
     {title: "personal", type: "dir", levelFromRoot: 1 },
   ],
   [
+    {title: "root", type: "dir", levelFromRoot: 0 },
     {title: "turing", type: "dir", levelFromRoot: 1 },
     {title: "0mod", type: "dir", levelFromRoot: 2 },
     {title: "capstone", type: "dir", levelFromRoot: 3 },
     {title: "1mod", type: "dir", levelFromRoot: 2 },
     {title: "personal", type: "dir", levelFromRoot: 1 },
     {title: "budget.txt", type: "file", levelFromRoot: 2 },
-    {title: "letter.txt", type: "file", levelFromRoot: 2 },
   ],
   [
+    {title: "root", type: "dir", levelFromRoot: 0 },
     {title: "turing", type: "dir", levelFromRoot: 1 },
     {title: "0mod", type: "dir", levelFromRoot: 2 },
-    {title: "practice", type: "dir", levelFromRoot: 3 },
-    {title: "game.rb", type: "file", levelFromRoot: 4 },
-    {title: "capstone", type: "dir", levelFromRoot: 3 },
+    {title: "practice1.js", type: "file", levelFromRoot: 3 },
+    {title: "practice2.js", type: "file", levelFromRoot: 3 },
     {title: "1mod", type: "dir", levelFromRoot: 2 },
-    {title: "classwork", type: "dir", levelFromRoot: 3 },
     {title: "practice", type: "dir", levelFromRoot: 3 },
-    {title: "2mod", type: "dir", levelFromRoot: 2 },
-    {title: "3mod", type: "dir", levelFromRoot: 2 },
+    {title: "classwork", type: "dir", levelFromRoot: 3 },
   ],
 ];
 
