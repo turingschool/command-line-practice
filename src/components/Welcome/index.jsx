33a50b72dee1c9832fd909e14819af3136a1b325
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-05-03 18:05:13 -0600

    Touch up content + styles

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -8,8 +8,8 @@ const Welcome = () => {
       <p>If you are new to using the terminal and want to start dipping your toes in, you are in the right place! This site will walk you through the basic commands a developer can start their journey with. You can interact with the content in multiple ways, but we recommend following this order:</p>
       <ul>
         <li><strong>Learn:</strong> Read about a set of commands - learn what they are used for and how to use them in your terminal</li>
-        <li><strong>PLayground:</strong> Interact with a terminal in this app to practice using the commands while you see a visual representation of the directories and files</li>
-        <li><strong>Challenges:</strong> Coming soon.</li>
+        <li><strong>Playground:</strong> Interact with a terminal on this site to practice using the commands while you see a visual representation of the directories and files</li>
+        <li><strong>Challenges:</strong> Complete three leveled challenges to practice your new skills and knowledge!</li>
       </ul>
     </main>
   );
